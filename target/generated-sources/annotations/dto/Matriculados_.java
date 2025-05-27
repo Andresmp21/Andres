20bd02dfa62a9cf1d3e06044b4f35c2201a5f22a
ALEXANDER <ALEXANDER@192.168.20.96>
2025-05-27 17:08:26 -0500

    Examen

---
+++ target/generated-sources/annotations/dto/Matriculados_.java
@@ -0,0 +1,21 @@
+package dto;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2025-05-27T15:33:37")
+@StaticMetamodel(Matriculados.class)
+public class Matriculados_ { 
+
+    public static volatile SingularAttribute<Matriculados, String> nombEstdWeb;
+    public static volatile SingularAttribute<Matriculados, String> ndniEstdWeb;
+    public static volatile SingularAttribute<Matriculados, String> apmaEstdWeb;
+    public static volatile SingularAttribute<Matriculados, Integer> codiEstdWeb;
+    public static volatile SingularAttribute<Matriculados, String> passEstd;
+    public static volatile SingularAttribute<Matriculados, Date> fechNaciEstdWeb;
+    public static volatile SingularAttribute<Matriculados, String> logiEstd;
+    public static volatile SingularAttribute<Matriculados, String> appaEstdWeb;
+
+}
